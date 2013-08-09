a11758c2969ea08affe179a8163b040f88991ea1
Ilya Boltnev <ilya@boltnev.com>
2013-08-09 19:43:15 +0400

    some code for words and documents database

--- indexer.py
+++ indexer.py
@@ -2,9 +2,12 @@
 #from nltk import tokenize
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Text, Integer, String
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///:memory:', echo=True)
+DBNAME = 'sqlite:///index.sqlite'
+
+engine = create_engine(DBNAME, echo=True)
 
 Base = declarative_base()
 
@@ -12,7 +15,7 @@ Base = declarative_base()
 class Word(Base):
   __tablename__ = 'words'
   id = Column(Integer, primary_key=True)
-  word  = Column(String)
+  word  = Column(String, index=True)
   count = Column(Integer)
   
   def __init__(self, word):
@@ -22,3 +25,23 @@ class Word(Base):
   def __repl__(self):
     return self.word
 
+  def count_inc():
+    self.count += 1
+
+#/* Base */
+class Document(Base):
+  __tablename__ = 'documents'
+  id = Column(Integer, primary_key=True)
+  text =  Column(Text)
+  category = Column(String)
+
+  def __init__(self, text, category):
+    self.text = text
+    self.category = category
+
+def start_session():
+  Session = sessionmaker(bind=engine) 
+  return Session()
+  
+
+Base.metadata.create_all(engine)
