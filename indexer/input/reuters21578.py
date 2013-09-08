21ba635de3d328be19ff0c797b5f58add247f23e
Ilya Boltnev <ilya@boltnev.com>
2013-09-08 16:16:13 +0400

    Reuters load and index.

--- indexer/input/reuters21578.py
+++ indexer/input/reuters21578.py
@@ -1,14 +1,19 @@
-from indexer.indexer import DBInterface
+from indexer.indexer import DBInterface, Document
 from indexer.models import reuters21578 
+import time
 
 class Reuters21578:     
     @staticmethod
     def load_corpus():
         reuters21578.load_corpus()
     
+    @staticmethod
     def index_corpus():
-        pass        
+        Document.index_all()        
         
 if __name__ == '__main__':
     DBInterface.recreate_base()
+    start = time.time()
     Reuters21578.load_corpus()
+    Reuters21578.index_corpus()
+    print 'Reuters corpus loading and indexing took ', time.time() - start, ' seconds'
